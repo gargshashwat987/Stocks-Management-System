a7b2657cf1ec65937a5d282b82f812388ca1d59e
gargshashwat987 <124812421+gargshashwat987@users.noreply.github.com>
2023-08-21 18:31:55 +0530

    Add files via upload

---
+++ interfaces/ProductOperations.java
@@ -0,0 +1,10 @@
+package interfaces;
+import java.lang.*;
+import classes.*;
+public interface ProductOperations
+{
+	void insertProduct(Product p);
+	void removeProduct(Product p);
+	Product getProduct(String pid);
+	void showAllProducts();
+}
